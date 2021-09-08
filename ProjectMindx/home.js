de1ab95dd4b1eb4781b6f2a6aa9045364c384cdd
doanduyank2001@gmail.com <doanduyank2001@gmail.com>
2021-09-08 14:08:08 +0700

    header + login

---
+++ ProjectMindx/home.js
@@ -0,0 +1,19 @@
+//Scrolling active
+window.addEventListener("scroll", function() {
+    let header = document.querySelector("header");
+    header.classList.toggle("scrolling-active", window.scrollY > 0);
+})
+
+//Drop down menu 
+let dropMenu = document.getElementById("myDropdown")
+let triggerMenu = document.getElementsByClassName("btn-book")[0];
+triggerMenu.onclick = function () {
+  dropMenu.style.display = "block";
+}
+
+window.addEventListener("mouseup", function(event) {
+  if(event.target != dropMenu) {
+    dropMenu.style.display = "none";
+  }
+})
+
