b933c43b55e8a1875e98a88a4486f5e9b7292377
duyanh712 <doanduyank2001@gmail.com>
2021-09-12 22:54:36 +0700

    update login

--- ProjectMindx/login.js
+++ ProjectMindx/login.js
@@ -4,13 +4,18 @@ document.getElementById("direct").onclick = function validuser () {
     //5-12 character letter or number
     var checkUser = /^[A-Za-z0-9]{5,12}$/;
     var userID = document.getElementById("username").value;
-    //Minimum eight characters, at least one letter, one number and one special character
+    //Minimum eight characters, at least one letter, one number 
     var checkPass = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
     var passID = document.getElementById("password").value;
-    if( !checkPass.test(passID))
-        alert("Please enter the correct validation user and pass")
-    else 
+    if(!checkUser.test(userID))
+        alert("Please enter the user 5-12 character");
+    else if(!checkPass.test(passID))
+        alert("Please enter password minimum eight characters, at least one letter, one number ")
+    else {
         document.location = "home-signed-in.html";
+        alert("Logged in successfully");
+    }
+        
 }
 
 
